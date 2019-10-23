d7b2984a6eef3b604c53f4d432f40f233185999c
hugo mancini <h.g.mancini@gmail.com>
2019-10-23 16:01:43 +0200

    order created and saved

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ require 'json'
 
 class PagesController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :connectApi
 
   def helloWorld
     puts 'hellow hugo'
@@ -12,14 +13,43 @@ class PagesController < ApplicationController
     street_address = params["street_address"]
     zip = params["zip"]
     city = params["city"]
-    country = params["country"]
     customerId = params["customer_id"]
-    customerName = params["customer_name"]
-    puts customerName
-    data = "API CALL OK !"
-    render json: {answer: data}
+    customerMail = params["customer_mail"]
+    email = params["email"]
+    line_items = JSON.parse(params["line_items"])
+    puts line_items
+    puts line_items.class
+    puts line_items[0]["variant_id"].class
+    puts line_items[0]["quantity"].class
+    variant_ids = []
+    line_items.each do |item|
+      variant_id = item["variant_id"].to_i
+      quantity = item["quantity"].to_i
+      n = { "quantity" => quantity, "variant_id" => variant_id }
+      variant_ids << n
+    end
+
+    create_order(variant_ids, customerId)
+
+    render json: {order: @order}
   end
 
+  def create_order(variant_ids, customerId)
+    @order = ShopifyAPI::Order.create(line_items: variant_ids, financial_status:"authorized", customer: { id: customerId })
+    @order.save
+    return @order
+  end
 
+  def index
+    @products = ShopifyAPI::Product.all
+  end
+
+  private
+
+  def connectApi
+    shop_url = "https://de69344ecda45841327dbd594af761e5:3623f41fc97008cf4f660757b0fe1acf@calicea.myshopify.com"
+    ShopifyAPI::Base.site = shop_url
+    ShopifyAPI::Base.api_version = '2019-10'
+  end
 
 end

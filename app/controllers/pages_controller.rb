0736ef2a92c7906b4268bb1c1f04c11a27c63fd8
Hugo MANCINI <h.g.mancini@gmail.com>
2019-11-18 11:00:36 +0100

    cip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,7 @@ class PagesController < ApplicationController
     pro_price = params['pro_price'].to_i
     total_price = (params['total_price'].to_i) / 100
     cip = params['cip']
+    puts cip
     line_json = params['line_items']
     line_items = JSON.parse(line_json)
     discount_amount = (total_price - pro_price).to_f
@@ -32,7 +33,7 @@ class PagesController < ApplicationController
   end
 
   def create_order(variant_ids, customer_id, code_name, discount_amount)
-    @order = ShopifyAPI::Order.new(line_items: variant_ids, note: cip, financial_status:"authorized", customer: { id: customer_id }, discount_codes:   [{
+    @order = ShopifyAPI::Order.new(line_items: variant_ids, financial_status:"authorized", customer: { id: customer_id }, discount_codes:   [{
     'code': "PROPRICE",
     'amount': "#{discount_amount}",
     'type': 'discount_code',

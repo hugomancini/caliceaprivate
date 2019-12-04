c0de8a820e16ea0f439efb44ec94ea713f65051b
Hugo MANCINI <h.g.mancini@gmail.com>
2019-12-04 14:40:48 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   post 'checkout_pro', to: "pages#checkout_pro"
   post 'delete_all_orders', to: "pages#delete_all_orders"
+  post 'create_pro_customer', to: "pages#create_pro_customer"
 
 end
 
